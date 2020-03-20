647ba89d49a4fe01b7031b653d2093eda3681848
David Zisowsky <David.Zisowsky@arvato.com>
2020-03-20 22:03:49 +0100

    added initial data

--- src/main/resources/db/migration/V1__INITIAL.sql
+++ src/main/resources/db/migration/V1__INITIAL.sql
@@ -1,4 +1,24 @@
-create table PERSON (
-    ID int not null,
-    NAME varchar(100) not null
+CREATE TABLE text_lang(
+    id BIGSERIAL PRIMARY KEY,
+    language_id BIGINT NOT NULL,
+    text TEXT NOT NULL
 );
+
+CREATE TABLE person(
+    id BIGSERIAL PRIMARY KEY,
+    uuid VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE questionnaire(
+    id BIGSERIAL PRIMARY KEY,
+    text_id BIGINT NOT NULL REFERENCES text_lang(id)
+);
+
+CREATE TABLE question(
+    id BIGSERIAL PRIMARY KEY,
+    questionnaire_id BIGINT NOT NULL REFERENCES questionnaire(id),
+    type BIGINT NOT NULL,
+    text_id BIGINT NOT NULL REFERENCES text_lang(id)
+);
+
+
