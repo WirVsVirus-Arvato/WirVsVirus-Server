4b8591d86da7ce481330f61488b2e6f777747791
Marvin Caspar <marvin.caspar@gmail.com>
2020-03-20 21:15:52 +0100

    Add db connection

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
+import org.flywaydb.gradle.FlywayExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.springframework.boot") version "2.2.5.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    id("org.flywaydb.flyway") version "6.3.1"
     kotlin("jvm") version "1.3.61"
     kotlin("plugin.spring") version "1.3.61"
 
@@ -19,13 +21,23 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.postgresql:postgresql:42.2.8")
+    implementation("org.flywaydb:flyway-core")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.codehaus.groovy:groovy-all:2.5.7")
     testImplementation("org.spockframework:spock-core:1.3-groovy-2.5")
     testImplementation("org.spockframework:spock-spring:1.3-groovy-2.5")
+}
+
+configure<FlywayExtension> {
+    url = "jdbc:postgresql://localhost:5432/corona-app"
+    user = "root"
+    password = "pw"
+    schemas = arrayOf("public")
 }
 
 //tasks.withType<Test> {
