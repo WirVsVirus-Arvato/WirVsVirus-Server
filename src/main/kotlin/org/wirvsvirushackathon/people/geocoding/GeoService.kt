f622b47ee35cac9e4ddf2f302d0931e3b20c7926
David Zisowsky <David.Zisowsky@arvato.com>
2020-03-22 17:11:58 +0100

    comment out geo code

--- src/main/kotlin/org/wirvsvirushackathon/people/geocoding/GeoService.kt
+++ src/main/kotlin/org/wirvsvirushackathon/people/geocoding/GeoService.kt
@@ -16,10 +16,10 @@ class GeoService() {
         headers.set("Accept", MediaType.APPLICATION_JSON_VALUE)
         val entity: HttpEntity<*> = HttpEntity<Any>(headers)
         val url = URLDecoder.decode ("https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?prox=51.67299%2C8.3446143%2C250&mode=retrieveAddresses&maxresults=1&gen=9&apiKey=hMMlYFxGvdEnS2fEbFt0ocxTH5Du4D7DWeZ-mZqLFc8")
-        var response = RestTemplate().getForObject(
-               url.toString(),
+ //       var response = RestTemplate().getForObject(
+   //            url.toString(),
 
-        )
+     //   )
 
         throw NotImplementedError()
 //        var firstAdress = response view?.get(0)?.result?.get(0)?.location?.address
